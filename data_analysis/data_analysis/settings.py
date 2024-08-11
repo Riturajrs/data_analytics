2e7694fce450e7a1a0d780c2ce976c6dd5cffb57
Rituraj Seal <rituraj2001.rs@gmail.com>
2024-08-11 23:17:40 +0530

    Added database connection details

--- data_analysis/data_analysis/settings.py
+++ data_analysis/data_analysis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'data_analysis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': config('HOST'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'NAME': config('NAME'),
     }
 }
 
